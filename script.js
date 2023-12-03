28601f6c1c3f09d1cd1c6ab9ac9faef9fb49fef1
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-12-03 06:48:03 -0800

    call back hell

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("view").innerText = "10";
+  setTimeout(() => {
+    document.getElementById("view").innerText = "9";
+    setTimeout(() => {
+      document.getElementById("view").innerText = "8";
+      setTimeout(() => {
+        document.getElementById("view").innerText = "7";
+        setTimeout(() => {
+          document.getElementById("view").innerText = "6";
+          setTimeout(() => {
+            document.getElementById("view").innerText = "5";
+            setTimeout(() => {
+              document.getElementById("view").innerText = "4";
+              setTimeout(() => {
+                document.getElementById("view").innerText = "3";
+                setTimeout(() => {
+                  document.getElementById("view").innerText = "2";
+                  setTimeout(() => {
+                    document.getElementById("view").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("view").innerText =
+                        "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
